d108e53719bb343c94d0f3833f9f14a8728d2466
norld <syfh@live.com>
2019-01-04 20:13:39 +0800

    添加轮询长度函数, 补充协议参数

--- FKDownloader/Major/Executor/FKSingleTask.m
+++ FKDownloader/Major/Executor/FKSingleTask.m
@@ -10,6 +10,28 @@
 #import "FKDownloadManager.h"
 #import "FKStorageHelper.h"
 
+typedef void(^finish)(long length, int idx);
+void pollingLength(NSArray *linkes, finish f) {
+    for (int i = 0; i < linkes.count; i++) {
+        NSString *link = [linkes objectAtIndex:i];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:link]];
+        request.HTTPMethod = @"HEAD";
+        [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            
+            NSNumber *length = @(0);
+            if ([response respondsToSelector:@selector(allHeaderFields)]) {
+                NSHTTPURLResponse *res = (NSHTTPURLResponse *)response;
+                NSDictionary *header = res.allHeaderFields;
+                NSNumber *temp = [[NSNumberFormatter new] numberFromString:[header valueForKey:@"Content-Length"]];
+                if (temp) { length = temp; }
+            }
+            if (f) {
+                f(length.longValue, i);
+            }
+        }] resume];
+    }
+}
+
 @interface FKSingleTask ()
 
 @property (nonatomic, strong) NSURLSessionDownloadTask *downloadTask;
@@ -18,15 +40,12 @@
 
 @implementation FKSingleTask
 @synthesize link = _link;
+@synthesize identifier = _identifier;
 @synthesize number = _number;
 @synthesize type = _type;
 
 - (void)start {
-    if (self.link.length) {
-        self.downloadTask = [self.manager.session downloadTaskWithURL:[NSURL URLWithString:self.link]];
-        [self.downloadTask resume];
-        [FKStorageHelper saveTask:self];
-    }
+    
 }
 
 - (void)suspend {
