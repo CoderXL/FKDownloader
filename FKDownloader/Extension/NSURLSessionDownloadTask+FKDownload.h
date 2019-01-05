2eaba7981bac3e068ed7e2f25aa89ac2c9835f69
norld <syfh@live.com>
2019-01-05 19:50:01 +0800

    标识 NSURLSessionDownloadTask

---
+++ FKDownloader/Extension/NSURLSessionDownloadTask+FKDownload.h
@@ -0,0 +1,17 @@
+//
+//  NSURLSessionDownloadTask+FKDownload.h
+//  FKDownloaderDemo
+//
+//  Created by norld on 2019/1/5.
+//  Copyright © 2019 Norld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURLSessionDownloadTask (FKDownload)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FKDownloader/Extension/NSURLSessionDownloadTask+FKDownload.m
@@ -0,0 +1,13 @@
+//
+//  NSURLSessionDownloadTask+FKDownload.m
+//  FKDownloaderDemo
+//
+//  Created by norld on 2019/1/5.
+//  Copyright © 2019 Norld. All rights reserved.
+//
+
+#import "NSURLSessionDownloadTask+FKDownload.h"
+
+@implementation NSURLSessionDownloadTask (FKDownload)
+
+@end
