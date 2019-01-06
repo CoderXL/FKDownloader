564242a248e3c61646746415d45c8990d71b3f74
norld <syfh@live.com>
2019-01-06 18:06:20 +0800

    简单创建TaskHub

---
+++ FKDownloader/Major/Manager/FKTaskHub.h
@@ -0,0 +1,17 @@
+//
+//  FKTaskHub.h
+//  FKDownloaderDemo
+//
+//  Created by norld on 2019/1/6.
+//  Copyright © 2019 Norld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FKTaskHub : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FKDownloader/Major/Manager/FKTaskHub.m
@@ -0,0 +1,13 @@
+//
+//  FKTaskHub.m
+//  FKDownloaderDemo
+//
+//  Created by norld on 2019/1/6.
+//  Copyright © 2019 Norld. All rights reserved.
+//
+
+#import "FKTaskHub.h"
+
+@implementation FKTaskHub
+
+@end
